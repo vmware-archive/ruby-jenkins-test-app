1228ea2a01f8a5c0443bbdc78f62e243a7021d80
Gerhard Lazu <gerhard@cloudcredo.com>
2014-08-20 16:39:31 +0100

    CF Dummy Broker test if set in VCAP_SERVICES
    Signed-off-by: Peter Ellis Jones <pete@peterellisjones.com>

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,13 +1,22 @@
 require "spec_helper"
 
+require "nokogiri"
+require "open-uri"
+require "ostruct"
+require "json"
+
+def cf_dummy_broker
+  JSON.parse(
+    ENV.fetch("VCAP_SERVICES", "{}")
+  ).fetch("cf-dummy-broker", []).first
+end
+
 describe "Jenkins Ruby Test App" do
   it "uses the Gemfile Ruby version" do
     expect(RUBY_VERSION).to eql("2.1.1")
   end
 
   it "Nokogiri is available" do
-    require "nokogiri"
-    require "open-uri"
 
     expect {
       Nokogiri::HTML(
@@ -15,4 +24,19 @@ describe "Jenkins Ruby Test App" do
       )
     }.to_not raise_exception
   end
+
+  if cf_dummy_broker
+    context "when cf-dummy-broker is available in VCAP_SERVICES env" do
+      let(:credentials) { OpenStruct.new(cf_dummy_broker.fetch("credentials")) }
+
+      it "can connect to this CF service instance" do
+        response = open(
+          "#{credentials.uri}/check_credentials",
+          http_basic_authentication: [credentials.username, credentials.password]
+        )
+        expect(response.status).to eql(["200", "OK"])
+      end
+    end
+  end
 end
+
